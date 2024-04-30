50bb9a73f76ce6ca6f48bd95d13cd6a7d5ae1180
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-04-30 18:39:20 +0530

    Add files via upload

---
+++ Untitled7 (1).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+year=int(input())
+if (year%4==0 and year%100!=0) or (year%400==0):
+    print("True")
+else:
+    print("False")
+
+
+# In[ ]:
+
+
+
+
